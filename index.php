b5f47ecb91d6d5ccbfdaea588bec10b7e91c86bf
Jesus Avelar <jesus.avelar.nav@gmail.com>
2023-07-07 09:10:31 -0600

    Cambios en rutas

--- index.php
+++ index.php
@@ -7,11 +7,8 @@ use Core\Whoops;
 
 Whoops::run();
 
-$requestURI = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-$requestMethod = $_SERVER['REQUEST_METHOD'];
-
 try {
-	Route::load('Routes/web.php')->call($requestURI, $requestMethod);
+	Route::load('Routes/web.php');
 } catch (Exception $exception) {
 	die($exception->getMessage());
 }
