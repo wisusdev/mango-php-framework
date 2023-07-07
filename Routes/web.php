9f0fa5f3000d27404989c05ebc15432a19be0027
Jesus Avelar <jesus.avelar.nav@gmail.com>
2023-07-07 09:09:57 -0600

    Rutas de ejemplo

--- Routes/web.php
+++ Routes/web.php
@@ -2,6 +2,7 @@
 
 $route = new Core\Route();
 
-$route->get("/", ["HomeController", "index"]);
-
-$route->get("/user/{id}", ["HomeController", "show"]);
+$route->add("/", ["HomeController", "index"]);
+$route->add("/home", ["HomeController", "index"]);
+$route->add("/user/{id}", ["HomeController", "show"]);
+$route->add("/download/{downID}/{filename}", ["HomeController", "show"]);
