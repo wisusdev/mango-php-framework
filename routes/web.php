da783c174f7ec8e93b2e69b25c55d991fd44bfe9
Jesus Avelar <jesus.avelar.nav@gmail.com>
2023-07-16 22:29:28 -0600

    Rutas para web

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,9 @@
 <?php
 
-$router = new Core\Route();
+use Core\Route;
 
-$router->add('/', ['HomeController', 'index']);
-$router->add('/install', ['HomeController', 'install']);
+Route::get('/', 'web/HomeController@index');
+Route::get('/install', 'web/HomeController@install');
 
-// Auth Endpoints
-$router->add('/api/login', ['UserController', 'login']);
-$router->add('/api/register', ['UserController', 'register']);
-$router->add('/api/password-reset', ['UserController', 'resetPassword']);
-
-// Lists Endpoints
-$router->add('/api/lists', ['ListController', 'create']);
-$router->add('/api/lists', ['ListController', 'update']);
-$router->add('/api/lists', ['ListController', 'add']);
-$router->add('/api/lists/items', ['ListController', 'addItem']);
+// Users
+Route::get('/user/{id}', 'web/UserController@show');
