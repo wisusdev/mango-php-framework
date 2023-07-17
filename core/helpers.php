7223affd2fd7d68c98760e1c403ccf1b48059490
Jesus Avelar <jesus.avelar.nav@gmail.com>
2023-07-16 22:38:41 -0600

    función dd

--- core/helpers.php
+++ core/helpers.php
@@ -1,7 +1,17 @@
 <?php
 
+use Core\Debug;
+
 function authedUser() {
-    $token = (new class { use \App\Traits\JWT; })->bearerToken();
+
+    $token = (new class { 
+        use \App\Traits\JWT; 
+    })->bearerToken();
 
     return (new \App\Models\User())->find(['token' => $token]);
+}
+
+function dd($data, $return = false): void
+{
+	Debug::var_dump($data, $return);
 }
